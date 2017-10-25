b738f222527955676faab6d3da1d9125c9cde848
Jae Hyeok Yoo <jaehyeokyoo@gmail.com>
2017-10-25 10:47:51 -0700

    compare data and simulation

--- sim.C
+++ sim.C
@@ -1,124 +1,204 @@
 void sim()
-{  
-    TH1D *hy = new TH1D("hy", "hy", 100, -0.17, 0.17);
-    TH1D *hz = new TH1D("hz", "hz", 100, -0.17, 0.17);
-    TH1D *hpx = new TH1D("hpx", "hpx", 100, 0, 100);
-    TH1D *hpy = new TH1D("hpy", "hpy", 100, -1, 1);
-    TH1D *hpz = new TH1D("hpz", "hpz", 100, -1, 1);
-    TH1D *htheta = new TH1D("htheta", "htheta", 100, 0*180/3.141592, 0.2*180/3.141592);
-    TH2D *hpxpt = new TH2D("hpxpt", "hpxpt", 100, 0, 100, 100, 0, 1);
-    TH2D *hpxtheta = new TH2D("hpxtheta", "hpxtheta", 100, 0, 100, 100, 0*180/3.141592, 0.2*180/3.141592);
-    TH2D *htop = new TH2D("htop", "htop", 100, -0.75, 0.75, 100, -0.75, 0.75);
-    TH2D *hbottom = new TH2D("hbottom", "hbottom", 100, -0.75, 0.75, 100, -0.75, 0.75);
-    TH2D *hsim = new TH2D("hsim", "hsim", 100, -0.75, 0.75, 100, -0.75, 0.75);
+{
+  gStyle->SetOptStat(0);
 
-    int intop=0;
-
-    ifstream fin("hits_45by45.txt");
-    string line;  
-    if(fin.is_open()) {
-        while(fin.good()){
-
-            // get a line from fin
-            getline(fin, line);  
-           // charge mass x y z (m) px py pz (MeV)
-           //  1.000000  105.000000  33.000000 -0.085899 0.076362  553.284552  -7.225200 8.656843
-        
-            float q, m; 
-            float x,y,z;
-            float px,py,pz;
-            
-            stringstream stream(line);
-            stream >> q;
-            stream >> m;
-            stream >> x;
-            stream >> y;
-            stream >> z;
-            stream >> px;
-            stream >> py;
-            stream >> pz; 
+  TH1D *h1_mc_occ = new TH1D("h1_mc_occ", "h1_mc_occ", 13, -0.5, 12.5);
+  TH1D *h1_mc_occ_layer = new TH1D("h1_mc_occ_layer", "h1_mc_occ_layer", 4, -0.5, 3.5);
+  TH1D *h1_mc_occ_cate = new TH1D("h1_mc_occ_cate", "h1_mc_occ_cate", 8, -0.5, 7.5);
   
-            hsim->Fill(y,z); 
+  TH1D *h1_data_occ = new TH1D("h1_data_occ", "h1_data_occ", 13, -0.5, 12.5);
+  TH1D *h1_data_occ_layer = new TH1D("h1_data_occ_layer", "h1_data_occ_layer", 4, -0.5, 3.5);
+  TH1D *h1_data_occ_cate = new TH1D("h1_data_occ_cate", "h1_data_occ_cate", 8, -0.5, 7.5);
 
-            // require bottom cross section
-            bool inBottom = false;
-            if (abs(y)<0.09 || abs(z-0.02)<0.045) inBottom = true;
-            if (!inBottom) continue;
-            hbottom->Fill(y,z); 
+  ifstream fin_mc("detectorHits.txt");
+  string line_mc;  
+  if(fin_mc.is_open()) {
+    while(fin_mc.good()){
 
-            // calculate cos theta
-            float deno = TMath::Sqrt(x*x + y*y + z*z)*TMath::Sqrt(px*px + py*py + pz*pz); 
-            float num = x*px + y*py + z*pz; 
-            //cout << q << " " << m << " " << x << " " << y << " " << z << " " << px << " " << py << " " << px << " "; 
-            //cout << num << "/" << deno << " " << TMath::ACos(num/deno)*180/3.141592<< endl;
-         
-            // calculate where it will hit on the top HS  
-            // assume 3.6 m from bottom and top HS  
-            float dy = 3.6*py/px;
-            float dz = 3.6*pz/px;
-            
-            bool inTop = false;
-            if(abs(y+dy)<0.045 || abs(z+dz)<0.045) inTop=true; 
-            if(abs(y+dy)>0.225 || abs(z+dz)>0.225) inTop=false; 
-            if(inTop) intop++;
+      // get a line from fin_mc
+      getline(fin_mc, line_mc);  
 
-            hy->Fill(y);
-            hz->Fill(z);
-            hpx->Fill(px/1000);
-            hpy->Fill(py/1000);
-            hpz->Fill(pz/1000);
-            htheta->Fill(TMath::ACos(num/deno)*180/3.141592);
-            hpxpt->Fill(px/1000, TMath::Sqrt(py*py+pz*pz)/1000);
-            hpxtheta->Fill(px/1000, TMath::ACos(num/deno)*180/3.141592); 
-            htop->Fill(y+dy,z+dz); 
+      if(line_mc.find("#")!=string::npos ) continue;
+      // charge mass x y z (m) px py pz (MeV) x y z (m) px py pz (MeV) */*/*/*/* 
+      // -1.0  105.0 33.0  -0.0377483330201  0.0340244299481 9196.10094566 -103.637254361  -6.0012614516 34.6627646749 -0.0551225667417  0.034966843793  8804.41796055 -91.1804012686  15.7343997837 2/-1/6 
 
-        }
+      float q, m; 
+      float ix,iy,iz;
+      float ipx,ipy,ipz;
+      float fx,fy,fz;
+      float fpx,fpy,fpz;
+      string hits;
+
+      stringstream stream(line_mc);
+      stream >> q;
+      stream >> m;
+      stream >> ix;
+      stream >> iy;
+      stream >> iz;
+      stream >> ipx;
+      stream >> ipy;
+      stream >> ipz; 
+      stream >> fx;
+      stream >> fy;
+      stream >> fz;
+      stream >> fpx;
+      stream >> fpy;
+      stream >> fpz; 
+      stream >> hits; 
+
+      if(q==111111) break;
+
+      // Get hits
+      bool hit[12]; 
+      for(int i=0; i<12; i++) hit[i]=0;
+      TString tstr_hits = hits;
+      TObjArray *tx = tstr_hits.Tokenize("/");
+      for (Int_t i = 0; i < tx->GetEntries(); i++)
+      {
+        TString hit_temp = ((TObjString *)(tx->At(i)))->String();
+        //cout << hit_temp << endl;
+        if(hit_temp=="0") hit[0]=1;
+        if(hit_temp=="1") hit[1]=1;
+        if(hit_temp=="2") hit[2]=1;
+        if(hit_temp=="3") hit[3]=1;
+        if(hit_temp=="4") hit[4]=1;
+        if(hit_temp=="5") hit[5]=1;
+        if(hit_temp=="6") hit[6]=1;
+        if(hit_temp=="7") hit[7]=1;
+        if(hit_temp=="8") hit[8]=1;
+        if(hit_temp=="9") hit[9]=1;
+        if(hit_temp=="10") hit[10]=1;
+        if(hit_temp=="11") hit[11]=1;
+      }
+
+
+      int Nocc = 0; 
+      for (Int_t i = 0; i < 12; i++)  Nocc = Nocc + hit[i];
+      h1_mc_occ->Fill(Nocc);
+
+      int occ_layer1 = hit[0]+hit[1]+hit[2]+hit[3];
+      int occ_layer2 = hit[4]+hit[5]+hit[6]+hit[7];
+      int occ_layer3 = hit[8]+hit[9]+hit[10]+hit[11];
+      h1_mc_occ_layer->Fill((occ_layer1>0)+(occ_layer2>0)+(occ_layer3>0));
+     
+      if(occ_layer1==0 && occ_layer2==0 && occ_layer3==0) h1_mc_occ_cate->Fill(0); 
+      if(occ_layer1>0  && occ_layer2==0 && occ_layer3==0) h1_mc_occ_cate->Fill(1); 
+      if(occ_layer1==0 && occ_layer2>0  && occ_layer3==0) h1_mc_occ_cate->Fill(2); 
+      if(occ_layer1==0 && occ_layer2==0 && occ_layer3>0 ) h1_mc_occ_cate->Fill(3); 
+      if(occ_layer1>0  && occ_layer2>0  && occ_layer3==0) h1_mc_occ_cate->Fill(4); 
+      if(occ_layer1>0  && occ_layer2==0 && occ_layer3>0 ) h1_mc_occ_cate->Fill(5); 
+      if(occ_layer1==0 && occ_layer2>0  && occ_layer3>0 ) h1_mc_occ_cate->Fill(6); 
+      if(occ_layer1>0  && occ_layer2>0  && occ_layer3>0 ) h1_mc_occ_cate->Fill(7); 
+
+      // look at only triple coincidence events in order to compare with data
+      if(occ_layer1>0  && occ_layer2>0  && occ_layer3>0 ) 
+      { 
+        // occ per layer : 2D of layer vs ch  (3x4)
+      } 
+
+      if(0)
+      {
+        //cout << q << " " << m << " " 
+        //     << ix << " " << iy << " " << iz << " "  
+        //     << ipx << " " << ipy << " " << ipz << " "  
+        //     << fx << " " << fy << " " << fz << " "  
+        //     << fpx << " " << fpy << " " << fpz << " "   
+        //     << hits << " ";
+        for (Int_t i = 0; i < 12; i++)  cout << hit[i];
+        cout << " "; 
+        cout << occ_layer1 << occ_layer2 << occ_layer3; 
+        cout << endl; 
+      }   
+
     }
+  }
+  fin_mc.close();
 
-    cout << intop << "/" << hy->Integral(-1,99999) << " " << intop/hy->Integral(-1,99999) << endl; 
+  // 
+  // Data
+  //
+  ifstream fin_data("data_mqtc.txt");
+  string line_data;  
+  if(fin_data.is_open()) {
+    while(fin_data.good()){
 
-    TCanvas *c = new TCanvas("c","c",1200,900);
-    c->Divide(4,3); 
-    c->cd(1);
-    htheta->Draw("hist ep");  
-    c->cd(1)->SetLogy(1);
-    c->cd(2);
-    c->cd(2)->SetLogy(1);
-    //hy->SetLineWidth(2);
-    //hy->SetLineColor(kBlack);
-    //hy->SetFillColor(kRed-3);
-    //hy->SetStats(0);
-    hy->Draw("hist ep");  
-    c->cd(3);
-    c->cd(3)->SetLogy(1);
-    hz->Draw("hist ep");  
-    c->cd(4);
-    c->cd(4)->SetLogy(1);
-    hpx->Draw("hist ep");  
-    c->cd(5);
-    c->cd(5)->SetLogy(1);
-    hpy->Draw("hist ep");  
-    c->cd(6);
-    c->cd(6)->SetLogy(1);
-    hpz->Draw("hist ep");  
-    c->cd(7);
-    c->cd(7)->SetLogz(1);
-    hpxpt->Draw("colz");  
-    c->cd(8);
-    c->cd(8)->SetLogz(1);
-    hpxtheta->Draw("colz");  
-    c->cd(9);
-    c->cd(9)->SetLogz(0);
-    htop->SetStats(0);  
-    htop->Draw("colz");  
-    c->cd(10);
-    c->cd(10)->SetLogz(0);
-    hbottom->SetStats(0);  
-    hbottom->Draw("colz");  
-    c->cd(11);
-    c->cd(11)->SetLogz(0);
-    hsim->SetStats(0);  
-    hsim->Draw("colz");  
+      // get a line from fin_data
+      getline(fin_data, line_data);  
 
-    c->Print("sim.pdf");
+      if(line_data.find("#")!=string::npos ) continue;
+      //2017-09-21 12:49:34 010110100100 
+
+      string date;
+      string time;
+      string hits;
+
+      stringstream stream(line_data);
+      stream >> date; 
+      stream >> time; 
+      stream >> hits; 
+      
+      if(date=="1111111") break;
+
+      // Get hits
+      bool hit[12]; 
+      for(int i=0; i<12; i++) 
+      { 
+        if(hits[i]=='1') hit[i]=1;
+        if(hits[i]=='0') hit[i]=0;
+      }
+
+      int Nocc = 0; 
+      for (Int_t i = 0; i < 12; i++)  Nocc = Nocc + hit[i];
+      h1_data_occ->Fill(Nocc);
+
+      int occ_layer1 = hit[0]+hit[1]+hit[2]+hit[3];
+      int occ_layer2 = hit[4]+hit[5]+hit[6]+hit[7];
+      int occ_layer3 = hit[8]+hit[9]+hit[10]+hit[11];
+      h1_data_occ_layer->Fill((occ_layer1>0)+(occ_layer2>0)+(occ_layer3>0));
+     
+      if(occ_layer1==0 && occ_layer2==0 && occ_layer3==0) h1_data_occ_cate->Fill(0); 
+      if(occ_layer1>0  && occ_layer2==0 && occ_layer3==0) h1_data_occ_cate->Fill(1); 
+      if(occ_layer1==0 && occ_layer2>0  && occ_layer3==0) h1_data_occ_cate->Fill(2); 
+      if(occ_layer1==0 && occ_layer2==0 && occ_layer3>0 ) h1_data_occ_cate->Fill(3); 
+      if(occ_layer1>0  && occ_layer2>0  && occ_layer3==0) h1_data_occ_cate->Fill(4); 
+      if(occ_layer1>0  && occ_layer2==0 && occ_layer3>0 ) h1_data_occ_cate->Fill(5); 
+      if(occ_layer1==0 && occ_layer2>0  && occ_layer3>0 ) h1_data_occ_cate->Fill(6); 
+      if(occ_layer1>0  && occ_layer2>0  && occ_layer3>0 ) h1_data_occ_cate->Fill(7); 
+
+      if(0)
+      {
+        cout << date << " " << time << " " 
+             << hits << " ";
+        for (Int_t i = 0; i < 12; i++)  cout << hit[i];
+        cout << " "; 
+        cout << occ_layer1 << occ_layer2 << occ_layer3; 
+        cout << endl; 
+      }   
+
+
+    }
+  }
+  fin_data.close();
+
+
+
+  TCanvas *c = new TCanvas("c","c",1200,900);
+  c->Divide(4,3); 
+  c->cd(1);
+  c->cd(1)->SetLogy(1);
+  h1_data_occ->SetMinimum(1);  
+  h1_data_occ->DrawNormalized("hist");  
+  h1_mc_occ->DrawNormalized("ep same");  
+  c->cd(2);
+  c->cd(2)->SetLogy(1);
+  h1_data_occ_layer->SetMinimum(1);  
+  h1_data_occ_layer->DrawNormalized("hist");  
+  h1_mc_occ_layer->DrawNormalized("ep same");  
+  c->cd(3);
+  c->cd(3)->SetLogy(1);
+  h1_data_occ_cate->SetMinimum(1);  
+  h1_data_occ_cate->DrawNormalized("hist");  
+  h1_mc_occ_cate->DrawNormalized("ep same");  
+  
+  c->Print("sim.pdf");
 }
