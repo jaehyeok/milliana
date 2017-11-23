4c7f974d49ddf89f3f3d7485c13f290181a20aa2
Jae Hyeok Yoo <jaehyeokyoo@gmail.com>
2017-11-23 11:30:52 -0800

    update

--- sim.C
+++ sim.C
@@ -1,5 +1,6 @@
 void sim()
 {
+  gStyle->SetPaintTextFormat("6.2f");
   gStyle->SetOptStat(0);
 
   TH1D *h1_mc_occ = new TH1D("h1_mc_occ", "h1_mc_occ", 13, -0.5, 12.5);
@@ -12,8 +13,10 @@ void sim()
   TH1D *h1_data_occ_layer = new TH1D("h1_data_occ_layer", "h1_data_occ_layer", 4, -0.5, 3.5);
   TH1D *h1_data_occ_cate = new TH1D("h1_data_occ_cate", "h1_data_occ_cate", 8, -0.5, 7.5);
   TH2D *h2_data_occ_cate = new TH2D("h2_data_occ_cate", "h2_data_occ_cate", 3, 0.5, 3.5, 4, 0.5, 4.5);
+  TH2D *h2_data_hit = new TH2D("h2_data_hit", "h2_data_hit", 25, -0.5, 24.5, 25, -0.5, 24.5);
   
 
+  //ifstream fin_mc("data/hits_withHodo.txt");
   ifstream fin_mc("data/hits_withSideHits.txt");
   string line_mc;  
   if(fin_mc.is_open()) {
@@ -75,17 +78,20 @@ void sim()
         if(hit_temp=="10") hit[10]=1;
         if(hit_temp=="11") hit[11]=1;
       }
-
-
-      int Nocc = 0; 
-      for (Int_t i = 0; i < 12; i++)  Nocc = Nocc + hit[i];
-      h1_mc_occ->Fill(Nocc);
-
+      
+      // number of hits per layer
       int occ_layer1 = hit[0]+hit[1]+hit[2]+hit[3];
       int occ_layer2 = hit[4]+hit[5]+hit[6]+hit[7];
       int occ_layer3 = hit[8]+hit[9]+hit[10]+hit[11];
       h1_mc_occ_layer->Fill((occ_layer1>0)+(occ_layer2>0)+(occ_layer3>0));
     
+      // select tripple coincidence events
+      if((occ_layer1==0) || (occ_layer2==0) || (occ_layer3==0)) continue;
+
+      int Nocc = 0; 
+      for (Int_t i = 0; i < 12; i++)  Nocc = Nocc + hit[i];
+      h1_mc_occ->Fill(Nocc);
+
       if(occ_layer1==0 && occ_layer2==0 && occ_layer3==0) { h1_mc_occ_cate->Fill(0); h2_mc_cate_pt->Fill(0., ipx/1000); }
       if(occ_layer1>0  && occ_layer2==0 && occ_layer3==0) { h1_mc_occ_cate->Fill(1); h2_mc_cate_pt->Fill(1., ipx/1000); }
       if(occ_layer1==0 && occ_layer2>0  && occ_layer3==0) { h1_mc_occ_cate->Fill(2); h2_mc_cate_pt->Fill(2., ipx/1000); }
@@ -122,11 +128,11 @@ void sim()
   }
   fin_mc.close();
 
-/*
+
   // 
   // Data
   //
-  ifstream fin_data("data_mqtc.txt");
+  ifstream fin_data("data/sync/sync_nov22.txt");
   string line_data;  
   if(fin_data.is_open()) {
     while(fin_data.good()){
@@ -137,16 +143,34 @@ void sim()
       if(line_data.find("#")!=string::npos ) continue;
       //2017-09-21 12:49:34 010110100100 
 
+      int beam;
+      int run;
+      int file; 
+      int event; 
       string date;
       string time;
+      string hs_hits1;
+      string hs_hits2;
+      string hs_hits3;
+      string hs_hits4;
+      string hs_hits5;
       string hits;
 
       stringstream stream(line_data);
+      stream >> beam; 
+      stream >> run; 
+      stream >> file; 
+      stream >> event; 
       stream >> date; 
       stream >> time; 
+      stream >> hs_hits1; 
+      stream >> hs_hits2; 
+      stream >> hs_hits3; 
+      stream >> hs_hits4; 
+      stream >> hs_hits5; 
       stream >> hits; 
       
-      if(date=="1111111") break;
+      if(date=="111111") break;
 
       // Get hits
       bool hit[12]; 
@@ -183,6 +207,20 @@ void sim()
         h2_data_occ_cate->Fill(3, occ_layer3);
       } 
 
+      // hit bot vs top 
+      float occ_hs_bot = 0;
+      float occ_hs_top = 0;
+      for(int i=0; i<8; i++) 
+      { 
+        if(hs_hits1[i]=='1') occ_hs_bot++;
+        if(hs_hits2[i]=='1') occ_hs_bot++;
+        if(hs_hits3[i]=='1') occ_hs_bot++;
+        if(hs_hits4[i]=='1') occ_hs_top++;
+        if(hs_hits5[i]=='1') occ_hs_top++;
+      }
+      //h2_data_hitfrac->Fill(occ_hs_bot/24., occ_hs_top/16.);
+      h2_data_hit->Fill(occ_hs_bot, occ_hs_top);
+
       if(0)
       {
         cout << date << " " << time << " " 
@@ -197,11 +235,6 @@ void sim()
     }
   }
   fin_data.close();
-<<<<<<< HEAD
-
-  //
-=======
-*/
 
 
   TCanvas *c = new TCanvas("c","c",1200,900);
@@ -213,8 +246,8 @@ void sim()
   h1_mc_occ->SetLineColor(kRed);  
   h1_mc_occ->SetLineWidth(2);  
   h1_data_occ->SetMinimum(1);  
-  h1_data_occ->DrawNormalized("hist");  
-  h1_mc_occ->DrawNormalized("hist same");  
+  h1_data_occ->DrawNormalized("e");  
+  h1_mc_occ->DrawNormalized("e same");  
   c->cd(2);
   c->cd(2)->SetLogy(1);
   h1_data_occ_layer->SetLineColor(kBlack);  
@@ -222,8 +255,8 @@ void sim()
   h1_mc_occ_layer->SetLineColor(kRed);  
   h1_mc_occ_layer->SetLineWidth(2);  
   h1_data_occ_layer->SetMinimum(1);  
-  h1_data_occ_layer->DrawNormalized("hist");  
-  h1_mc_occ_layer->DrawNormalized("hist same");  
+  h1_data_occ_layer->DrawNormalized("e");  
+  h1_mc_occ_layer->DrawNormalized("e same");  
   c->cd(3);
   c->cd(3)->SetLogy(1);
   h1_data_occ_cate->SetLineColor(kBlack);  
@@ -231,19 +264,22 @@ void sim()
   h1_mc_occ_cate->SetLineColor(kRed);  
   h1_mc_occ_cate->SetLineWidth(2);  
   h1_data_occ_cate->SetMinimum(1);  
-  h1_data_occ_cate->DrawNormalized("hist");  
-  h1_mc_occ_cate->DrawNormalized("hist same");  
+  h1_data_occ_cate->DrawNormalized("e");  
+  h1_mc_occ_cate->DrawNormalized("e same");  
   c->cd(4);
   c->cd(4)->SetLogz(1); 
   h2_mc_occ_cate->SetMarkerSize(2);
-  h2_mc_occ_cate->Draw("colz text");
+  h2_mc_occ_cate->DrawNormalized("colz text");
   c->cd(5);
   c->cd(5)->SetLogz(1); 
   h2_data_occ_cate->SetMarkerSize(2);
-  h2_data_occ_cate->Draw("colz text");
+  h2_data_occ_cate->DrawNormalized("colz text");
   c->cd(6);
   c->cd(6)->SetLogz(1);
-  h2_mc_cate_pt->Draw("colz text"); 
+  h2_mc_cate_pt->DrawNormalized("colz text"); 
+  c->cd(7);
+  c->cd(7)->SetLogz(1);
+  h2_data_hit->Draw("colz text");
 
-  c->Print("data_sim.pdf");
+  c->Print("fig/data_sim.pdf");
 }
